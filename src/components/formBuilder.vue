3fc876d590f85a858d58c8cb3a412909224c3e63
vishal vaghasiya <vishalvaghasiya.me@gmail.com>
2020-06-08 18:25:11 +0530

    Form Builder in import Form Elements

--- src/components/formBuilder.vue
+++ src/components/formBuilder.vue
@@ -12,6 +12,8 @@
                        :data-vv-as="field.label">
             </component>
         </div>
+
+        {{formValues}}
         <!--                <div>
                             <button type="submit" :disabled="disabled">submit</button>
                         </div>-->
@@ -20,27 +22,40 @@
 
 <script>
     import Vue from 'vue';
+    import Input from "../FormElements/Input";
+    import Checkbox from "../FormElements/Checkbox";
+    import RadioButton from "../FormElements/RadioButton";
 
     export default {
         name: "formBuilder",
         props: ['config'],
+        components: {
+            Input,
+            Checkbox,
+            RadioButton
 
+        },
         data() {
             return {
-                formValues: '',
+                formValues: {},
             }
         },
         /*
           computed: {
-                            disabled() {
-                                return Object.keys(this.fields).some(key => this.fields[key].invalid)
-                            }
-                        },*/
+            disabled() {
+              return Object.keys(this.fields).some(key => this.fields[key].invalid)
+              }
+          },*/
 
         created() {
             this.config.map(f => {
                 Vue.set(this.formValues, f.name, null)
             })
+        },
+        methods: {
+            updateField(field, value) {
+                this.formValues[field] = value;
+            }
         }
     }
 </script>
